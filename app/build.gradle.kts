517d333890a96aacc4ae347abea533983ce1b1b7
sebasdelalv340 <148891336+sebasdelalv340@users.noreply.github.com>
2025-01-24 12:40:20 +0100

    dependencias kps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -57,4 +59,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //Room
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+
+    ksp("androidx.room:room-compiler:2.5.0")
 }
